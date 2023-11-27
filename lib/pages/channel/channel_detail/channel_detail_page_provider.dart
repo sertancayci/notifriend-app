200dcd9e3fb77294ba57bd7f747d81b309e9e110
sertancayci <sertancayci@gmail.com>
2023-11-27 23:11:12 +0300

    wip

--- lib/pages/channel/channel_detail/channel_detail_page_provider.dart
+++ lib/pages/channel/channel_detail/channel_detail_page_provider.dart
@@ -16,18 +16,16 @@ class ChannelDetailNotifier extends StateNotifier<ChannelDetailState>
     state = state.copyWith(channelDetail: channelDetail);
   }
 
-  Future<void> fetchChannelNotifications(int? channelId) async {
-    if (channelId == null) {
-      return;
-    }
-    final channelDetail = await channelService.getChannelDetail(channelId);
+  Future<void> fetchChannelNotifications(int channelId) async {
+   final channelNotification = await notificationService.getChannelNotifications(channelId);
 
-    state = state.copyWith(channelDetail: channelDetail);
+    state = state.copyWith(channelNotification: channelNotification);
   }
 
   Future<void> init(int? categoryId) async {
     state = state.copyWith(isLoading: true);
     await fetchChannelDetail(categoryId!);
+    await fetchChannelNotifications(channelId);
     state = state.copyWith(isLoading: false);
   }
 }
@@ -36,19 +34,23 @@ class ChannelDetailState {
   ChannelDetailState({
     this.isLoading,
     this.channelDetail,
+    required this.channelNotification,
   });
 
   final ChannelResponse? channelDetail;
   final bool? isLoading;
+  final NotificationResponse channelNotification;
 
   ChannelDetailState copyWith(
       {List<NotificationResponse>? notifications,
       bool? isLoading,
       ChannelResponse? channelDetail,
+        List<NotificationResponse>? channelNotification
       List<ChannelResponse>? forYouChannels}) {
     return ChannelDetailState(
       channelDetail: channelDetail ?? this.channelDetail,
       isLoading: isLoading ?? this.isLoading,
+      channelNotification: channelNotification ?? this.channelNotification,
     );
   }
 }
